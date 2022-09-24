3b761ff4264381aa8ae1eeb0983fa83d3b152a8d
Eyal Toledano <shenron@Eyals-MacBook-Pro.local>
2022-09-24 00:48:38 -0400

    Adjustments to meta data

--- app/root.tsx
+++ app/root.tsx
@@ -16,17 +16,20 @@ export function links() {
 
 export const meta: MetaFunction = () => {
   const description = `The wizzies have released a handy tool to help warm your Doodles and celebrate in the elements! Right click and save your Triwizzy Doodle today, compliments of the Ministry of Wizard Doodles.`
+  const image = "https://wvvmijmjchpqjqiwewxy.supabase.co/storage/v1/object/public/general/wizzyup",
   return {
     charset: "utf-8",
     title: "Wizzy up your Doodle!",
     viewport: "width=device-width,initial-scale=1",
-    description,
-    "twitter:image": "https://wvvmijmjchpqjqiwewxy.supabase.co/storage/v1/object/public/general/wizzyup",
+    "og:description": description,
+    "og:image": image,
+    "twitter:image": image,
     "twitter:card": "summary_large_image",
     "twitter:creator": "@eyaltoledano",
     "twitter:site": "@wizzyministry",
     "twitter:title": "Wizzy up your Doodle!",
     "twitter:description": description,
+    description,
   }
 };
 
